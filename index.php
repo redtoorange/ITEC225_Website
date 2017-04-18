eb887fd2110bf4574dbac6c67db03b00418be35e
redtoorange <redtoorange@gmail.com>
2017-04-18 07:55:40 -0400

    Commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+    <title>Andrew McGuiness</title>
+</head>
+<style>
+.repos{
+    margin-bottom: 25px;
+}
+.repos ul{
+    margin: 0px;
+    list-style-type: square;
+}
+.repos a:link {color: #000000; text-decoration: underline; }
+.repos a:active {color: #0000ff; text-decoration: underline; }
+.repos a:visited {color: #008000; text-decoration: underline; }
+.repos a:hover {color: #ff0000; text-decoration: none; }
+</style>
+<body>
+    <h1>Nothing much to see here</h1>
+    
+    <div class="repos">
+        <a href="https://github.com/redtoorange">Github</a>
+        <ul>
+            <li>
+                <a href="https://github.com/redtoorange/ProjectFour">Live Flight Monitoring</a>
+            </li>
+            
+            <li>
+                <a href="https://github.com/redtoorange/RVUCalcuator">Hospital RVU Tracker</a>
+            </li>
+        </ul>
+    </div>
+    
+    
+    <div class="repos">
+        <a href="https://bitbucket.org/redtoorange/">BitBucket</a>
+        <ul>
+            <li>
+                <a href="https://bitbucket.org/redtoorange/loancalculator">401K and Loan Calculator</a>
+            </li>
+            
+            <li>
+                <a href="https://bitbucket.org/redtoorange/outlive">Current Game Project</a>
+            </li>
+            
+            <li>
+                <a href="https://bitbucket.org/redtoorange/gb-roguelike">GB-Jam Entry</a>
+            </li>
+            
+            <li>
+                <a href="https://bitbucket.org/redtoorange/ecs-lite">
+                Entity Component System Library</a>
+            </li>
+        </ul>
+    </div>
+    
+</body>
+</html>
